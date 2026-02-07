1d69c7d4de135e9287fb2885d3f4df03fd79db53
tbohn2 <tanner.bohn@yahoo.com>
2026-02-06 21:24:39 -0700

    Changed style of login container

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -2,7 +2,7 @@ import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-primary px-4">
+    <div className="absolute top-1/2 -translate-y-1/2 w-full flex justify-center items-center">
       <LoginForm />
     </div>
   );
